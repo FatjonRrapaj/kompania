a7ccb51ae35353d7b266fe7272512ab386042879
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-03-20 12:02:14 +0100

    fonts & colors config

--- constants/Colors.ts
+++ constants/Colors.ts
@@ -1,19 +1,126 @@
-const tintColorLight = '#2f95dc';
-const tintColorDark = '#fff';
+const primary500 = "#00C27C";
+const primary90 = `${primary500}90`;
+const primary80 = `${primary500}80`;
+const primary20 = `${primary500}20`;
+const primary10 = `${primary500}10`;
+const primary5 = `${primary500}5`;
+
+export const primary = {
+  500: primary500,
+  90: primary90,
+  80: primary80,
+  20: primary20,
+  10: primary10,
+  5: primary5,
+};
+
+const secondary500 = "#A3D139";
+const secondary90 = `${secondary500}90`;
+const secondary80 = `${secondary500}80`;
+const secondary20 = `${secondary500}20`;
+const secondary10 = `${secondary500}10`;
+const secondary5 = `${secondary500}5`;
+
+export const secondary = {
+  500: secondary500,
+  90: secondary90,
+  80: secondary80,
+  20: secondary20,
+  10: secondary10,
+  5: secondary5,
+};
+
+const tertiary500 = "#A862EA";
+const tertiary90 = `${tertiary500}90`;
+const tertiary80 = `${tertiary500}80`;
+const tertiary20 = `${tertiary500}20`;
+const tertiary10 = `${tertiary500}10`;
+const tertiary5 = `${tertiary500}5`;
+
+export const tertiary = {
+  500: tertiary500,
+  90: tertiary90,
+  80: tertiary80,
+  20: tertiary20,
+  10: tertiary10,
+  5: tertiary5,
+};
+
+const dark500 = "#161F1C";
+const dark90 = `${dark500}90`;
+const dark80 = `${dark500}80`;
+const dark20 = `${dark500}20`;
+const dark10 = `${dark500}10`;
+const dark5 = `${dark500}5`;
+
+export const dark = {
+  500: dark500,
+  90: dark90,
+  80: dark80,
+  20: dark20,
+  10: dark10,
+  5: dark5,
+};
+
+const gray500 = "#A3ADAA";
+const gray90 = `${gray500}90`;
+const gray80 = `${gray500}80`;
+const gray20 = `${gray500}20`;
+const gray10 = `${gray500}10`;
+const gray5 = `${gray500}5`;
+
+export const gray = {
+  500: gray500,
+  90: gray90,
+  80: gray80,
+  20: gray20,
+  10: gray10,
+  5: gray5,
+};
+
+const light500 = "#D9E1E1";
+const light90 = `${light500}90`;
+const light80 = `${light500}80`;
+const light20 = `${light500}20`;
+const light10 = `${light500}10`;
+const light5 = `${light500}5`;
+
+export const light = {
+  500: light500,
+  90: light90,
+  80: light80,
+  20: light20,
+  10: light10,
+  5: light5,
+};
+
+const white500 = "#FFFFFF";
+const white90 = `${white500}90`;
+const white80 = `${white500}80`;
+const white20 = `${white500}20`;
+const white10 = `${white500}10`;
+const white5 = `${white500}5`;
+
+export const white = {
+  500: white500,
+  90: white90,
+  80: white80,
+  20: white20,
+  10: white10,
+  5: white5,
+};
+
+//TODO: Gradients?
 
 export default {
   light: {
-    text: '#000',
-    background: '#fff',
-    tint: tintColorLight,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorLight,
+    text: dark500,
+    background: white500,
+    tabIconStroke: dark500,
   },
   dark: {
-    text: '#fff',
-    background: '#000',
-    tint: tintColorDark,
-    tabIconDefault: '#ccc',
-    tabIconSelected: tintColorDark,
+    text: white500,
+    background: dark500,
+    tabIconStroke: white500,
   },
 };
