0afbb1e8a0d426486e975ca85ebc838d28eecf21
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-03-23 09:49:42 +0100

    pressable ok declaration

--- components/Pressable.tsx
+++ components/Pressable.tsx
@@ -2,12 +2,19 @@ import * as React from "react";
 import {
   Pressable as DefaultPressable,
   PressableProps as DefaultPressableProps,
+  StyleProp,
+  ViewStyle,
 } from "react-native";
 
-const Pressable = ({ style, ...rest }: any) => {
+interface PressableProps extends DefaultPressableProps {}
+
+const Pressable = ({ style, ...rest }: PressableProps) => {
   return (
     <DefaultPressable
-      style={({ pressed }) => [{ opacity: pressed ? 0.7 : 1.0 }, style]}
+      style={({ pressed }) => [
+        { opacity: pressed ? 0.7 : 1.0 },
+        style as StyleProp<ViewStyle>,
+      ]}
       {...rest}
     />
   );
