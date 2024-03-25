cf34529c8f26efa981d2fc5481311e748f20ddea
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-03-25 11:26:38 +0100

    rm unused var from firbase.ts config

--- store/auth.ts
+++ store/auth.ts
@@ -1,1 +1,6 @@
- 
+import { create } from "zustand";
+import { immer } from "zustand/middleware/immer";
+import { Immutable } from "immer";
+import { User } from "firebase/auth";
+
+import { callLogin } from "@/api/auth";
