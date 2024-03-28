7949da5da8b4aaf37ce1f20548e0f5aae41a045b
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-03-28 16:11:43 +0100

    adding doc reference

--- api/auth.ts
+++ api/auth.ts
@@ -1,10 +1,27 @@
 import { signInWithEmailAndPassword, signOut } from "firebase/auth";
 
-import { auth } from "@/utils/firebase";
+import { auth, db } from "@/utils/firebase";
 import generateCustomError from "@/utils/customError";
+import { doc } from "firebase/firestore";
 
 const getUserId = () => {
   return auth?.currentUser?.uid;
+};
+
+const getUserDocumentReference = (uid?: string) => {
+  try {
+    let uidToUse = uid;
+    if (!uid) {
+      //get current uid
+      uidToUse = auth.currentUser?.uid;
+    }
+    if (!uidToUse) {
+      throw new Error("Something went wrong");
+    }
+    return doc(db, "drivers", uidToUse);
+  } catch (error) {
+    throw error;
+  }
 };
 
 export const callLogin = async (info: UserLoginInfo): Promise<void> => {
