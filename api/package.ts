5e6977f75e79874b89847699c3ce166326d344e6
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-03-29 09:57:06 +0100

    getting current and prev period

--- api/package.ts
+++ api/package.ts
@@ -1,3 +1,8 @@
+import { doc, getDoc } from "firebase/firestore";
+import { GeoPoint } from "firebase/firestore";
+import { Collections } from "@/constants/Firestore";
+import { db } from "@/utils/firebase";
+
 import { Customer } from "./company";
 
 export type CurrencyShortValue = "ALL" | "EUR";
@@ -23,4 +28,60 @@ export interface Package {
   price: number;
   status: PackageStatus;
   uid?: string;
+}
+
+type MonthHalfPeriod = 1 | 2;
+interface PackagesPeriod {
+  year: number;
+  month: number;
+  monthHalfPeriod: MonthHalfPeriod;
+  packagesPeriodName: string;
+}
+
+function getPeriod(date?: Date): PackagesPeriod {
+  const dateToUse = date ?? new Date();
+  const year = dateToUse.getFullYear();
+  const month = dateToUse.getMonth() + 1;
+  const day = dateToUse.getDate();
+  let monthHalfPeriod: MonthHalfPeriod = day <= 15 ? 1 : 2;
+
+  const packagesPeriodName = `packages-${year}-${month
+    .toString()
+    .padStart(2, "0")}-${monthHalfPeriod}`;
+
+  return {
+    year,
+    month,
+    monthHalfPeriod,
+    packagesPeriodName,
+  };
+}
+
+function getPreviousPeriod(): PackagesPeriod {
+  const {
+    year: currentYear,
+    month: currentMonth,
+    monthHalfPeriod: currentMonthHalfPeriod,
+  } = getPeriod();
+
+  let prevYear = currentMonth === 1 ? currentYear - 1 : currentYear;
+  let prevMonth =
+    currentMonthHalfPeriod === 1 ? currentMonth - 1 : currentMonth;
+  if (prevMonth === 0) {
+    //it means the current month is january, and we need to reset to december
+    prevMonth = 12;
+  }
+  let prevMonthHalfPeriod: MonthHalfPeriod =
+    currentMonthHalfPeriod === 2 ? 1 : 2;
+
+  let prevPackagesPeriodName = `packages-${prevYear}-${prevMonth
+    .toString()
+    .padStart(2, "0")}-${prevMonthHalfPeriod}`;
+
+  return {
+    year: prevYear,
+    month: prevMonth,
+    monthHalfPeriod: prevMonthHalfPeriod,
+    packagesPeriodName: prevPackagesPeriodName,
+  };
 }
