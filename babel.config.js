5c4159c0c92f2eb3476ed1d1ab2c27d55c95fbb3
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-04-12 11:26:32 +0200

    android, ios, web

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,21 @@
 module.exports = function (api) {
+  const platform = api.caller((caller) => caller?.platform);
   api.cache(true);
   return {
-    presets: ['babel-preset-expo']
+    presets: ["babel-preset-expo"],
+    plugins:
+      platform === "web"
+        ? [
+            ["@babel/plugin-proposal-decorators", { legacy: true }],
+            ["@babel/plugin-proposal-class-properties", { loose: true }],
+            [
+              "@babel/plugin-transform-runtime",
+              {
+                helpers: true,
+                regenerator: true,
+              },
+            ],
+          ]
+        : [],
   };
 };
