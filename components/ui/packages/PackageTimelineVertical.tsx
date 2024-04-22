98e23fbc1f6c9c8350057a4a1594b21ba4329e7b
Fatjon Rrapaj <fatjonrrapaj@live.com>
2024-04-22 14:21:33 +0200

    building the timeline

--- components/ui/packages/PackageTimelineVertical.tsx
+++ components/ui/packages/PackageTimelineVertical.tsx
@@ -4,6 +4,7 @@ import { StyleSheet } from "react-native";
 import { View } from "@/components/Themed";
 import { PackageTimeline } from "@/api/package";
 import { gray, primary } from "@/constants/Colors";
+import { Body2, Caption } from "@/components/StyledText";
 
 const PackageTimelineDash = () => (
   <View
@@ -19,21 +20,34 @@ const PackageTimelineDash = () => (
 
 interface PackageTimelineCirclesProps {
   isActive: boolean;
+  timestamp: number;
+  actionDescription: string;
 }
 
-const PackageTimelineCircles = ({ isActive }: PackageTimelineCirclesProps) => {
+const PackageTimelineCircles = ({
+  isActive,
+  timestamp,
+  actionDescription,
+}: PackageTimelineCirclesProps) => {
   const backgroundColor = isActive ? primary : gray;
   return (
-    <View style={[styles.outerCircle, { backgroundColor: backgroundColor[5] }]}>
+    <View style={styles.circlesContainer}>
       <View
-        style={[styles.midCircle, { backgroundColor: backgroundColor[10] }]}
+        style={[styles.outerCircle, { backgroundColor: backgroundColor[5] }]}
       >
         <View
-          style={[
-            styles.innerCircle,
-            { backgroundColor: backgroundColor[500] },
-          ]}
-        />
+          style={[styles.midCircle, { backgroundColor: backgroundColor[10] }]}
+        >
+          <View
+            style={[
+              styles.innerCircle,
+              { backgroundColor: backgroundColor[500] },
+            ]}
+          />
+        </View>
+      </View>
+      <View style={styles.circlesTimeContainer}>
+        <Caption style={{ color: gray[500] }}>Time {timestamp}</Caption>
       </View>
     </View>
   );
@@ -43,13 +57,42 @@ interface PackageTimelineVerticalProps {
   timeline: PackageTimeline;
 }
 
-const PackageTimelineVertical = (props: PackageTimelineVerticalProps) => {
-  return <View></View>;
+const PackageTimelineVertical = ({
+  timeline,
+}: PackageTimelineVerticalProps) => {
+  const [shortVersion, setShortVersion] = useState(false);
+  const timelineArray: any = useMemo(() => {
+    let timelineArr = Object.keys(timeline)
+      .filter((k) => !!timeline[k])
+      .sort((t1) => ((timeline[t2] as number) - timeline[t1]) as number)
+      .map((k) => timeline[k]);
+    if (shortVersion) {
+      return timelineArr.slice(0, 2);
+    } else return timelineArr;
+  }, [shortVersion]);
+
+  return (
+    <View>
+      <Body2>{timeline.createdAtDate}</Body2>
+      {timelineArray.map((timestamp) => (
+        <>
+          <PackageTimelineCircles isActive={true} />
+          <PackageTimelineDash />
+        </>
+      ))}
+    </View>
+  );
 };
 
 export default PackageTimelineVertical;
 
 const styles = StyleSheet.create({
+  container: { alignItems: "center" },
+  circlesContainer: { flexDirection: "row", alignItems: "center", gap: 16 },
+  circlesTimeContainer: {
+    position: "relative",
+    alignItems: "flex-start",
+  },
   outerCircle: {
     width: 24,
     height: 24,
